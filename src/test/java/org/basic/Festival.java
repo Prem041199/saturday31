41cbe6d0243a2e7ae997840c1676545518b1975e
Prem041199 <premkumarmettur@gmail.com>
2022-12-31 11:47:40 +0530

    saturdays first code

---
+++ src/test/java/org/basic/Festival.java
@@ -0,0 +1,22 @@
+package org.basic;
+
+public class Festival {
+	private void Pongal() {
+		System.out.println("January");
+	}
+	private void Tamilnewyear() {
+		System.out.println("April");
+	}
+	private void Vinayagarsathurthi() {
+		System.out.println("October");
+
+	}
+	private void Vijayadhasamy() {
+		System.out.println("october");
+
+	}
+	private void deepavali() {
+	System.out.println("November");
+	}
+
+}
